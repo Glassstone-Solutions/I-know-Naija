584ddd5c05a358cba20d8280f2a9a52ed8969db9
Thompson Edolo <verygreenboi@live.com>
2015-10-19 14:50:23 +0100

    Add Utils and assets for DispatchActivity

---
+++ app/src/main/java/ng/codehaven/game/iknownaija/ui/views/TextField.java
@@ -0,0 +1,19 @@
+package ng.codehaven.game.iknownaija.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import ng.codehaven.game.iknownaija.R;
+
+/**
+ * Created by chris on 17/03/15.
+ * For Calligraphy.
+ */
+public class TextField extends TextView {
+
+    public TextField(final Context context, final AttributeSet attrs) {
+        super(context, attrs, R.attr.textFieldStyle);
+    }
+
+}
