4298ecfbdb120ff94842512472ec5a820266ccac
Thompson Edolo <verygreenboi@live.com>
2015-10-31 17:39:26 +0100

    Version bump

--- app/src/main/java/ng/codehaven/game/iknownaija/ui/GameActivity.java
+++ app/src/main/java/ng/codehaven/game/iknownaija/ui/GameActivity.java
@@ -1,5 +1,6 @@
 package ng.codehaven.game.iknownaija.ui;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -14,6 +15,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.snapshot.Snapshot;
 import com.squareup.otto.Bus;
 
 import org.json.JSONArray;
@@ -33,7 +39,39 @@ import ng.codehaven.game.iknownaija.models.Quiz;
 import ng.codehaven.game.iknownaija.ui.adapters.AnswerAdapter;
 import ng.codehaven.game.iknownaija.ui.adapters.AnswerAdapter.AnswerInterface;
 
-public class GameActivity extends BaseActivity implements OnClickListener, AnswerInterface {
+public class GameActivity extends BaseActivity implements OnClickListener, AnswerInterface, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = GameActivity.class.getSimpleName();
+
+    // The AppState slot we are editing.  For simplicity this sample only manipulates a single
+    // Cloud Save slot and a corresponding Snapshot entry,  This could be changed to any integer
+    // 0-3 without changing functionality (Cloud Save has four slots, numbered 0-3).
+    private static final int APP_STATE_KEY = 0;
+
+    // Request code used to invoke sign-in UI.
+    private static final int RC_SIGN_IN = 9001;
+
+    // Request code used to invoke Snapshot selection UI.
+    private static final int RC_SELECT_SNAPSHOT = 9002;
+
+    /// Client used to interact with Google APIs.
+    private GoogleApiClient mGoogleApiClient;
+
+    // Progress Dialog used to display loading messages.
+    private ProgressDialog mProgressDialog;
+
+    // True when the application is attempting to resolve a sign-in error that has a possible
+    // resolution,
+    private boolean mIsResolving = false;
+
+    // True immediately after the user clicks the sign-in button/
+    private boolean mSignInClicked = false;
+
+    // True if we want to automatically attempt to sign in the user at application start.
+    private boolean mAutoStartSignIn = true;
+
+    private Snapshot mSnapshot;
 
     @InjectView(R.id.txt_countdown)
     TextView mCountdownView;
@@ -99,6 +137,32 @@ public class GameActivity extends BaseActivity implements OnClickListener, Answe
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
         mRecycler.setAdapter(adapter);
 
+        // Build API client with access to Games, AppState, and SavedGames.
+        // It is very important to add Drive or the SavedGames API will not work
+        // Make sure to also go to console.developers.google.com and enable the Drive API for your
+        // project
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES) // Games
+                .addScope(Drive.SCOPE_APPFOLDER) // SavedGames
+                .build();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+//        updateUI();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
@@ -242,4 +306,18 @@ public class GameActivity extends BaseActivity implements OnClickListener, Answe
         });
     }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
